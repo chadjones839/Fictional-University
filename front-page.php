4962f307320744b592d94fe7e80e8ad8f45d9493
Chad Jones <chadjones839@gmail.com>
2021-06-08 15:31:16 -0500

    create + display relationships

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
     <h1 class="headline headline--large">Welcome!</h1>
     <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
     <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-    <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+    <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
   </div>
 </div>
 
@@ -48,7 +48,7 @@
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
               <p><?php if (has_excerpt()) {
-                the_excerpt();
+                echo get_the_excerpt();
               } else {
                 echo wp_trim_words(get_the_content(), 15);
               }; ?> <a href="<?php the_permalink(); ?>"class="nu gray">Learn more</a></p>
--- header.php
+++ header.php
@@ -22,8 +22,8 @@
               <a href="<?php echo site_url('/about-us'); ?>">About Us</a>
             </li>
 
-            <li>
-              <a href="<?php echo site_url('/programs'); ?>">Programs</a>
+            <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"'; ?>>
+              <a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a>
             </li>
 
             <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"'; ?>>
